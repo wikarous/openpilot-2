4ec1d3cfa51920a3949459c04ff09f3edd12a05e
wikarous <67613740+wikarous@users.noreply.github.com>
2021-01-05 23:16:08 +0900

    Update hyundaican.py
    소나타 터보

--- selfdrive/car/hyundai/hyundaican.py
+++ selfdrive/car/hyundai/hyundaican.py
@@ -36,10 +36,15 @@ def create_lkas11(packer, car_fingerprint, bus, apply_steer, steer_req, cnt, ena
     values["CF_Lkas_HbaOpt"] = lkas11["CF_Lkas_HbaOpt"] if keep_stock else 1
     values["CF_Lkas_FcwOpt_USM"] = lkas11["CF_Lkas_FcwOpt_USM"] if keep_stock else 2
     values["CF_Lkas_LdwsOpt_USM"] = lkas11["CF_Lkas_LdwsOpt_USM"] if keep_stock else 0
-  if car_fingerprint == CAR.KIA_OPTIMA:
+  elif car_fingerprint == CAR.KIA_OPTIMA:
     values["CF_Lkas_Bca_R"] = 0
     values["CF_Lkas_HbaOpt"] = lkas11["CF_Lkas_HbaOpt"] if keep_stock else 1
     values["CF_Lkas_FcwOpt_USM"] = lkas11["CF_Lkas_FcwOpt_USM"] if keep_stock else 0
+  elif car_fingerprint == CAR.SONATA_LF_TURBO:
+    values["CF_Lkas_FcwOpt_USM"] = 2 if enabled else 1
+    values["CF_Lkas_LdwsOpt_USM"] = 2
+    values["CF_Lkas_FcwOpt_USM"] = 2 if enabled else 1
+    values["CF_Lkas_SysWarning"] = 4 if sys_warning else 0
 
   dat = packer.make_can_msg("LKAS11", 0, values)[2]
 
